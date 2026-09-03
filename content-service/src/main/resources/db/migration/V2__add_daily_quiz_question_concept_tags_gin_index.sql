80a523381c0a8223767721ef29e4f8fdf3380b43
wontae <dnjsxo0616@gmail.com>
2026-09-04 00:52:35 +0900

    chore: Daily Quiz 개념 태그 GIN 인덱스 적용 연기 #44

--- content-service/src/main/resources/db/migration/V2__add_daily_quiz_question_concept_tags_gin_index.sql
+++
@@ -1,6 +0,0 @@
--- Daily Quiz 문제은행에서 ACTIVE 문항을 개념 태그로 재사용 조회할 때 사용한다.
--- 기본 jsonb_ops GIN 인덱스는 JSONB 배열의 ?| 연산자를 지원한다.
-CREATE INDEX idx_daily_quiz_questions_active_concept_tags
-    ON content_schema.p_daily_quiz_questions
-    USING GIN (concept_tags)
-    WHERE status = 'ACTIVE';
