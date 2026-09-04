a846c80ebb7e0798c9b30f47db409c4302a67ad2
wontae <dnjsxo0616@gmail.com>
2026-09-04 23:55:38 +0900

    feat: Daily Quiz 세트 생성 결과와 개념 슬롯 선정 구현 #101

---
+++ content-service/src/main/java/com/maesamco/content/dailyquiz/application/service/DailyQuizSetGenerationStatus.java
@@ -0,0 +1,12 @@
+package com.maesamco.content.dailyquiz.application.service;
+
+public enum DailyQuizSetGenerationStatus {
+    // 세트 생성 완료
+    CREATED,
+    // 해당 사용자의 오늘 퀴즈가 이미 존재
+    ALREADY_EXISTS,
+    // 슬롯을 구성할 수 있는 개념이 존재하지 않음
+    NO_AVAILABLE_CONCEPTS,
+    // 최소 문항 미달
+    INSUFFICIENT_QUESTIONS
+}
