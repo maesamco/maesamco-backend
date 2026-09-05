75e81f6327b680f1d00c1f80b271d7866476050c
jjangjun0 <junyoung5915@gmail.com>
2026-09-05 20:08:02 +0900

    response 객체 작성

---
+++ content-service/src/main/java/com/maesamco/content/problem/presentation/dto/response/ProblemResponse.java
@@ -0,0 +1,55 @@
+package com.maesamco.content.problem.presentation.dto.response;
+
+import com.maesamco.content.problem.domain.entity.Problem;
+import com.maesamco.content.problem.domain.enums.ProblemDifficulty;
+import com.maesamco.content.problem.domain.enums.ProblemSource;
+import com.maesamco.content.problem.domain.enums.ProblemStatus;
+import com.maesamco.content.problem.domain.enums.ProblemType;
+import com.maesamco.content.problem.domain.enums.ProgrammingLanguage;
+import com.maesamco.content.problem.domain.enums.TimerPolicy;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+/**
+ * 문제 단건 조회 응답 DTO
+ * <p>[문제 클릭했을 때 상세 화면에 보여줄 정보]</p>
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProblemResponse {
+
+    private final UUID id;
+    private final String title;
+    private final ProgrammingLanguage language;
+    private final ProblemDifficulty difficulty;
+    private final ProblemType type;
+    private final String description;
+    private final String starterCode;
+    private final Integer runningTimeLimit;
+    private final Integer runningMemoryLimit;
+    private final TimerPolicy timerPolicy;
+    private final ProblemSource source;
+    private final ProblemStatus problemStatus;
+    private final Integer currentVersionNo;
+
+    public static ProblemResponse from(Problem problem) {
+        return new ProblemResponse(
+                problem.getId(),
+                problem.getTitle(),
+                problem.getLanguage(),
+                problem.getDifficulty(),
+                problem.getType(),
+                problem.getDescription(),
+                problem.getStarterCode(),
+                problem.getRunningTimeLimit(),
+                problem.getRunningMemoryLimit(),
+                problem.getTimerPolicy(),
+                problem.getSource(),
+                problem.getProblemStatus(),
+                problem.getCurrentVersionNo()
+        );
+    }
+}
