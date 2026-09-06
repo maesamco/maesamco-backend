294cfd33b3b7e0a77e40c18439ee62c6de4f6447
jjangjun0 <junyoung5915@gmail.com>
2026-09-07 01:37:18 +0900

    문제에서 특수한 경우에 대한 조회를 구현하기 위한 파일

---
+++ content-service/src/main/java/com/maesamco/content/problem/infrastructure/persistence/ProblemQueryRepository.java
@@ -0,0 +1,16 @@
+package com.maesamco.content.problem.infrastructure.persistence;
+
+/**
+ * 문제 조회 전용 Querydsl Repository
+ *
+ * <p>복잡한 조회 로직을 담당한다.</p>
+ *
+ * <ul>
+ *  <li>인기 문제 TOP 10 조회</li>
+ *  <li>최근 생성 문제 조회</li>
+ *  <li>특정 커리큘럼에 포함된 문제 조회</li>
+ *  <li>태그 + 풀이 통계를 JOIN한 복합 조회</li>
+ *  </ul>
+ */
+public class ProblemQueryRepository {
+}
