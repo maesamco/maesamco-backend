ba38d938cccb5c5814a47d7e2c4968302322b574
jjangjun0 <junyoung5915@gmail.com>
2026-09-07 14:33:47 +0900

    fix: 불필요한 import 삭제

--- content-service/src/main/java/com/maesamco/content/problem/domain/entity/Problem.java
+++ content-service/src/main/java/com/maesamco/content/problem/domain/entity/Problem.java
@@ -11,7 +11,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
