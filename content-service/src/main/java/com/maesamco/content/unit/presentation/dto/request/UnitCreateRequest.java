95e77b4b542ecfc71f14f0c1e7f2641120797772
jjangjun0 <junyoung5915@gmail.com>
2026-09-07 09:34:19 +0900

    unit request / response

---
+++ content-service/src/main/java/com/maesamco/content/unit/presentation/dto/request/UnitCreateRequest.java
@@ -0,0 +1,32 @@
+package com.maesamco.content.unit.presentation.dto.request;
+
+import com.maesamco.content.curriculum.domain.enums.ProgrammingLanguage;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class UnitCreateRequest {
+
+    /** 커리큘럼 식별자 */
+    @NotNull
+    private UUID curriculumId;
+
+    /** 유닛 제목 */
+    @NotBlank
+    @Size(max = 100)
+    private String title;
+
+    /** 프로그래밍 언어 */
+    @NotNull
+    private ProgrammingLanguage language;
+
+    /** 유닛 표시 순서 */
+    @NotNull
+    private Integer displayOrder;
+}
---
+++ content-service/src/main/java/com/maesamco/content/unit/presentation/dto/request/UnitUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.maesamco.content.unit.presentation.dto.request;
+
+import com.maesamco.content.curriculum.domain.enums.ProgrammingLanguage;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UnitUpdateRequest {
+
+    /** 유닛 제목 */
+    @Size(max = 100)
+    private String title;
+
+    /** 프로그래밍 언어 */
+    private ProgrammingLanguage language;
+
+    /** 유닛 표시 순서 */
+    private Integer displayOrder;
+}
