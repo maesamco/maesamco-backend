c5fa127062bac760ab5877588bdfbabc34cfa922
jjangjun0 <junyoung5915@gmail.com>
2026-09-08 05:34:51 +0900

    fix: Problem Progress Entity만 일차적으로 추가

---
+++ content-service/src/main/java/com/maesamco/content/problem/domain/entity/ProblemProgress.java
@@ -0,0 +1,64 @@
+package com.maesamco.content.problem.domain.entity;
+
+import com.maesamco.content.problem.domain.enums.ProgressStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/** 문제 풀이 진행 엔티티 */
+@Entity
+@Getter
+@Table(name = "p_problem_progress")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProblemProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private UUID userId;
+
+    @Column(name = "problem_id", nullable = false, updatable = false)
+    private UUID problemId;
+
+    @Column(name = "version_no", nullable = false)
+    private Integer versionNo;
+
+    @Column(name = "solved_at")
+    private Instant solvedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "progress_status", nullable = false, length = 20)
+    private ProgressStatus progressStatus;
+
+    private ProblemProgress(UUID userId, UUID problemId, Integer versionNo, ProgressStatus progressStatus) {
+        this.userId = userId;
+        this.problemId = problemId;
+        this.versionNo = versionNo;
+        this.progressStatus = progressStatus;
+    }
+
+    /** 문제 풀이 진행 정보를 생성한다. */
+    public static ProblemProgress create(UUID userId, UUID problemId, Integer versionNo) {
+        return new ProblemProgress(userId, problemId, versionNo, ProgressStatus.NOT_ATTEMPTED);
+    }
+
+    /** 채점 기준 문제 버전을 변경한다. */
+    public void changeVersionNo(Integer versionNo) { this.versionNo = versionNo; }
+
+    /** 문제 풀이 상태를 변경한다. */
+    public void changeProgressStatus(ProgressStatus progressStatus) { this.progressStatus = progressStatus; }
+
+    /** 문제를 최초로 시각을 기록한다. */
+    public void firstSolved() {
+        this.solvedAt = Instant.now();
+        this.progressStatus = ProgressStatus.CORRECT;
+    }
+}
