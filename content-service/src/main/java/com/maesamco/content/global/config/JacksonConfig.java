4c5f6eff0f581536d3394c7b7ead4c84ae307544
jjangjun0 <junyoung5915@gmail.com>
2026-09-08 21:39:48 +0900

    fix: JsonNullable 모듈 활성화

---
+++ content-service/src/main/java/com/maesamco/content/global/config/JacksonConfig.java
@@ -0,0 +1,15 @@
+package com.maesamco.content.global.config;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+}
