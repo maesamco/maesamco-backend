a9fef8fa35df51b67c59ea0e4ca5a8841a39417a
coco <meunse1999@gmail.com>
2026-09-09 21:56:49 +0900

    test: @AutoConfigureMockMvc 추가

--- judge-service/src/test/java/com/maesamco/judge/presentation/internal_controller/SubmissionInternalControllerTest.java
+++ judge-service/src/test/java/com/maesamco/judge/presentation/internal_controller/SubmissionInternalControllerTest.java
@@ -16,11 +16,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(SubmissionInternalController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class SubmissionInternalControllerTest {
 
     @Autowired
