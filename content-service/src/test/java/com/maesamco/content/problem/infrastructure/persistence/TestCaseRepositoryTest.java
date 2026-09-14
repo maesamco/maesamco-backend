5e40320cb4571b44192808fbdae536e330ca5d78
jjangjun0 <junyoung5915@gmail.com>
2026-09-14 19:18:50 +0900

    refactor: problem 관련 정상 동작하는 test (4차)

--- content-service/src/test/java/com/maesamco/content/problem/infrastructure/persistence/TestCaseRepositoryTest.java
+++ content-service/src/test/java/com/maesamco/content/problem/infrastructure/persistence/TestCaseRepositoryTest.java
@@ -1,20 +1,13 @@
 package com.maesamco.content.problem.infrastructure.persistence;
 
+import com.maesamco.content.domain.problem.entity.Problem;
+import com.maesamco.content.domain.problem.enums.*;
+import com.maesamco.content.domain.problem.repository.ProblemRepository;
+import com.maesamco.content.domain.testcase.entity.TestCase;
+import com.maesamco.content.domain.testcase.enums.TestCaseStatus;
+import com.maesamco.content.domain.testcase.repository.TestCaseRepository;
 import com.maesamco.content.global.config.JpaAuditingConfig;
 import com.maesamco.content.global.config.QuerydslConfig;
-import com.maesamco.content.problem.domain.entity.Problem;
-import com.maesamco.content.problem.domain.enums.ProblemDifficulty;
-import com.maesamco.content.problem.domain.enums.ProblemSource;
-import com.maesamco.content.problem.domain.enums.ProblemStatus;
-import com.maesamco.content.problem.domain.enums.ProblemType;
-import com.maesamco.content.problem.domain.enums.ProgrammingLanguage;
-import com.maesamco.content.problem.domain.enums.RunningMemoryLimit;
-import com.maesamco.content.problem.domain.enums.RunningTimeLimit;
-import com.maesamco.content.problem.domain.enums.TimerPolicy;
-import com.maesamco.content.problem.domain.repository.ProblemRepository;
-import com.maesamco.content.testcase.domain.entity.TestCase;
-import com.maesamco.content.testcase.domain.enums.TestCaseStatus;
-import com.maesamco.content.testcase.domain.repository.TestCaseRepository;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
