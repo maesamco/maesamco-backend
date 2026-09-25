155e1c256d04e856483e117054f89c289c0bfe50
wontae <dnjsxo0616@gmail.com>
2026-09-25 12:16:26 +0900

    refactor: 일일 퀴즈 배치의 중복 스케줄링 설정 제거 #297

--- content-service/src/main/java/com/maesamco/content/infrastructure/dailyquiz/scheduler/DailyQuizBatchConfig.java
+++ content-service/src/main/java/com/maesamco/content/infrastructure/dailyquiz/scheduler/DailyQuizBatchConfig.java
@@ -3,7 +3,6 @@ package com.maesamco.content.infrastructure.dailyquiz.scheduler;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
 
@@ -13,7 +12,6 @@ import java.time.Clock;
  */
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(DailyQuizBatchProperties.class)
-@EnableScheduling
 public class DailyQuizBatchConfig {
 
     /**
